d3c7b3bdcbcedc84b3c9d8f6aa9016e76da7f7a4
DiegoAleksis <diegoarh19@gmail.com>
2022-05-09 20:48:35 -0500

    Creacion de la clase hija Circulo, que hereda de Figura ademas de la creacion del super de relleno y color, con el constructor que incluye radio

---
+++ src/Circulo.java
@@ -0,0 +1,24 @@
+public abstract class Circulo extends Figura {
+
+
+    public Circulo(String color, boolean relleno) {
+        super(color, relleno);
+    }
+    private double radio;
+
+    public Circulo(String color, boolean relleno, double radio) {
+        super(color, relleno);
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+}
+
+
