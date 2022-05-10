ce93f3b67ddfc60317a212ad1f80a351891d3bb6
DiegoAleksis <diegoarh19@gmail.com>
2022-05-09 19:46:49 -0500

    Clase 09/05/22

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        //ToDo Clase padre Figura, va a ser abstract
+        //Llevara un  string color, un atributo  boolean relleno
+        //Constructor que reciba los 2 parametros, que son color y relleno
+        //Getters Setters
+        //2 metodos abstractos, obtener area que me retorne un double y habra otro llamado obtener_perimetro,
+        // sera abstracto y retornara double
+
+
+
+        //ToDo  Clase Circulo
+        //atributo radio que sera un double
+        //Constructor hereda super, e incluye radio
+        //getter setters
+        //Implementar obtenerarea, implementar obtenerperimetro
+
+
+
+        //ToDo Clase Rectangulo
+        //Tendra 2 atributos doubles, que seran ancho y alto
+        //Constructor que hereda a super e incluye ancho y alto
+        //getters y setters
+        //Implementar obtenerarea, implementar obtenerperimetro
+
+    }
+}
